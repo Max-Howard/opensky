23ecd5d3146b23c95ee046d306978dc863adace3
Max Howard <mdeh2@cam.ac.uk>
2024-11-09 17:55:54 +0000

    Add .gitignore and update flight data retrieval to use flightlist method

--- test.py
+++ test.py
@@ -2,17 +2,10 @@ from pyopensky.trino import Trino
 import pandas as pd
 
 trino = Trino()
-# full description of the whole set of parameters in the documentation
-# trino.flightlist(start, stop, *, airport, callsign, icao24)
-# trino.history(start, stop, *, callsign, icao24, bounds)
-# trino.rawdata(start, stop, *, callsign, icao24, bounds)
 
-start = "2021-09-01T00:00:00Z"
-stop = "2021-09-02T00:00:00Z"
+start = "2022-09-01T00:00:00Z"
+stop = "2022-09-02T00:00:00Z"
 
-flights = trino.history(start, stop)
-print("Number of flights:", len(flights))
+df = trino.flightlist(start, stop, limit=100000)
 
-# Since flights is already a dataframe, you can directly save it to a CSV file
-flights.to_csv('flights.csv', index=False)
-
+df.to_csv('output/flights.csv', index=False)
