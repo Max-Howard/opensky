e5687e9564a917aa1bdb8e0ac333cac9f3314c52
Max Howard <mdeh2@cam.ac.uk>
2024-11-11 23:05:19 +0000

    data cleaned on inport, import list moved to csv

--- import_test_flights.py
+++ import_test_flights.py
@@ -1,48 +1,68 @@
 from pyopensky.trino import Trino
 from datetime import datetime, timedelta
+import pandas as pd
+import numpy as np
 
 trino = Trino()
 
 scanning_stop = datetime.now()
-scanning_start = scanning_stop - timedelta(days=10)
+scanning_start = scanning_stop - timedelta(days=100)
 REQUESTED_COLUMNS = ["time", "lat", "lon", "velocity", "heading", "vertrate", "onground", "baroaltitude", "geoaltitude", "lastposupdate", "lastcontact", "icao24"]
+PATH_FLIGHTS_TO_LOAD = "fcounts_sample.csv"
 
-pairs_to_test = {
-    "heathrow_denver": {"departure":"EGLL","arrival": "KDEN"},
-    "glasgow_amsterdam" : {"departure" : "EGPF", "arrival" : "EHAM"}
-    }
+# pairs_to_test = {
+#     {"name": "heathrow_denver", "departure":"EGLL","arrival": "KDEN", "typecode": "A140", "count": 1},
+#     {"name": "glasgow_amsterdam", "departure" : "EGPF", "arrival" : "EHAM", "typecode": None, "count": 1}}
 
-def test_flight_data_from_list(pairs_to_test):
-    for pair_name in pairs_to_test.keys():
-        flight = pairs_to_test[pair_name]
-        df = trino.flightlist(
+def test_flight_data_from_list():
+    flights_to_import = pd.read_csv(PATH_FLIGHTS_TO_LOAD)
+    airport_names = pd.read_csv("airports.csv")
+    flights_to_import["origin_name"] = flights_to_import["origin"].map(airport_names.set_index("icao")["name"])
+    flights_to_import["destination_name"] = flights_to_import["destination"].map(airport_names.set_index("icao")["name"])
+    for i in range(1,len(flights_to_import)):
+        flight_to_import = flights_to_import.iloc[i]
+        if flight_to_import["typecode"] == np.nan:
+            print(f"""Searching for {flight_to_import["count"]} flight(s) from {flight_to_import["origin_name"]} to {flight_to_import["destination_name"]} with typecode {flight_to_import['typecode']}""")
+        else:
+            print(f"""Searching for {flight_to_import["count"]} flight(s) from {flight_to_import["origin_name"]} to {flight_to_import["destination_name"]} without typecode""")
+
+        # Trino seems to handle the case where the typecode is None, so we can just pass it in
+        flight_start_end = trino.flightlist(
             scanning_start,
             scanning_stop,
-            departure_airport=flight["departure"],
-            arrival_airport=flight["arrival"],
-            limit=1)
-        data_start = df["firstseen"][0]
-        data_stop = df["lastseen"][0]
-        icao24 = df["icao24"][0]
-        print(f"Found flight for: {pair_name}, loading data from {data_start} to {data_stop}")
+            departure_airport=flight_to_import["origin"],
+            arrival_airport=flight_to_import["destination"],
+            icao24=flight_to_import["typecode"],
+            limit=flight_to_import["count"])
 
-        df = trino.history(start=data_start,
-                        stop=data_stop,
-                        icao24=icao24,
-                        selected_columns=REQUESTED_COLUMNS)
-        df.to_csv(f'output/{pair_name}.csv', index=False)
-        print(f"Saved {pair_name}.csv")
+        if type(flight_start_end) is not pd.DataFrame:
+            print("Could not find flight.\n")
+            continue
 
-def import_flightlist(pairs_to_test):
-    for pair_name in pairs_to_test.keys():
-            flight = pairs_to_test[pair_name]
-            df = trino.flightlist(
-                scanning_start,
-                scanning_stop,
-                departure_airport=flight["departure"],
-                arrival_airport=flight["arrival"],
-                limit=10)
-            df.to_csv(f'output/lists/{pair_name}.csv', index=False)
+        print(f"Found {len(flight_start_end)} flights from {flight_to_import['origin_name']} to {flight_to_import['destination_name']}")
+        print(flight_start_end)
 
-test_flight_data_from_list(pairs_to_test)
+        for i in range(len(flight_start_end)):
+            data_start = flight_start_end.iloc[i, flight_start_end.columns.get_loc("firstseen")]
+            data_stop = flight_start_end.iloc[i, flight_start_end.columns.get_loc("lastseen")]
+
+            print(f"Loading path for flight number {i+1}, time period: {data_start} to {data_stop}")
+
+            flight_path = trino.history(start=data_start,
+                            stop=data_stop,
+                            icao24=flight_start_end.iloc[i, flight_start_end.columns.get_loc("icao24")],
+                            selected_columns=REQUESTED_COLUMNS)
+            initial_length = len(flight_path)
+            flight_path = flight_path.dropna().reset_index(drop=True)   # Drop rows with missing data otherwise sorting will fill with NaN
+            flight_path = flight_path[flight_path['lastposupdate'].diff() != 0].reset_index(drop=True)  # Drop rows with duplicate lastposupdate
+            final_length = len(flight_path)
+            rows_removed = initial_length - final_length
+            if rows_removed > 0:
+                print(f"Removed {rows_removed} rows due to missing or repeated data")
+            flight_path = flight_path.sort_values(by='time') # TODO not sure why this is necessary
+            filename = f"""output/{flight_to_import["origin"]}_{flight_to_import["destination"]}_{i+1}.csv"""
+            flight_path.to_csv(filename, index=False)
+            print(f"""Saved {flight_to_import["origin_name"]} to {flight_to_import["destination_name"]}, flight number {i+1} as {filename}\n""")
+
+test_flight_data_from_list()
 
--- plot_flight_path.py
+++ plot_flight_path.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
 import os
 from geopy.distance import geodesic
 import numpy as np
@@ -13,49 +14,30 @@ def load_flight_paths():
             if file.endswith('.csv'):
                   file_path = os.path.join('./output', file)
                   df = pd.read_csv(file_path)
-                  initial_length = len(df)
-                  df = df.dropna().reset_index(drop=True)   # Drop rows with missing data otherwise sorting will fill with NaN
-                  final_length = len(df)
-                  rows_removed = initial_length - final_length
-                  print(f"Removed {rows_removed} rows with missing data in flight path {file}\n")
-                  df = df.sort_values(by='time') # TODO not sure why this is necessary
                   flight_paths[file.replace('.csv', '')] = df
-
       return flight_paths
 
 
-def analyse_data(flight_paths, time_gap_thresh=60, vel_mismatch_thresh=10, vel_sample_size=200, replace_vel_with_speed=False, position_update_thresh=30):
-      anomalies = {"neg_time": {"lats": [], "lons": []},
-                   "large_time_gap": {"lats": [], "lons": []},
-                   "vel_mismatch": {"lats": [], "lons": []}}
+def analyse_data(flight_paths, time_gap_thresh=60, vel_mismatch_thresh=10, vel_sample_size=100):
+      vel_mismatch_lat = []
+      vel_mismatch_lon = []
       for flight_path_name, flight_path in flight_paths.items():
-
-            initial_length = len(flight_path)
-            flight_path = flight_path.dropna().reset_index(drop=True)
-            final_length = len(flight_path)
-            rows_removed = initial_length - final_length
-            print(f"Removed {rows_removed} rows with missing data in flight path {flight_path_name}\n")
-
             times = pd.to_datetime(flight_path['time'])
-            last_pos_update = pd.to_datetime(flight_path['lastposupdate'], unit='s', origin='unix', utc=True)
             lats = flight_path['lat']
             lons = flight_path['lon']
-            
+
             for i in range(1, len(times)):
                   time_gap = (times[i] - times[i - 1]).total_seconds()
                   distance_travelled = np.round(geodesic((lats[i - 1], lons[i - 1]), (lats[i], lons[i])).kilometers)
                   if time_gap < 0:
-                        anomalies["neg_time"]["lats"].append(lats[i])
-                        anomalies["neg_time"]["lons"].append(lons[i])
-                        print(f"""Negative time jump in flight path {flight_path_name}, between indexes {i-1} and {i}.\n"""
-                              f"""Initial time: {times[i-1]}, final time: {times[i]}\n""")
+                        print(f"""Negative time jump in flight path {flight_path_name}, between indexes {i-1} and {i}.\n""")
                   elif time_gap > time_gap_thresh:
-                        anomalies["large_time_gap"]["lats"].append(lats[i])
-                        anomalies["large_time_gap"]["lons"].append(lons[i])
                         print(f"""Large time gap in flight path {flight_path_name}, between indexes {i-1} and {i}.\n"""
                               f"""Time gap: {time_gap} seconds, distance gap: {distance_travelled} km\n""")
-                  if i % vel_sample_size == 0:
-                        # continue
+                  elif distance_travelled / time_gap - flight_path['velocity'][i] > 10:   # Velocity check over 1 index
+                        print(f"Velocity mismatch at index {i} in {flight_path_name}.\n"
+                              f"Calculated velocity: {distance_travelled / time_gap} m/s, reported velocity: {flight_path['velocity'][i]} m/s\n")
+                  elif i % vel_sample_size == 0:                                          # Every vel_sample_size indexes, check velocity over sample
                         time_gap = (times[i] - times[i - vel_sample_size]).total_seconds()
                         distance_travelled = 0
                         for j in range(i - vel_sample_size, i):
@@ -65,35 +47,21 @@ def analyse_data(flight_paths, time_gap_thresh=60, vel_mismatch_thresh=10, vel_s
                         speed_step = np.round((displacement * 1000 / time_gap))
                         velocity_step = np.round((distance_travelled * 1000 / time_gap))
                         velocity_reported = np.round(np.mean(flight_path['velocity'][i-vel_sample_size:i]))
-                        if time_gap > time_gap_thresh * vel_sample_size or time_gap < 0:
-                              continue # Skip velocity check if time gap is too large or negative time gap
-                        elif (times[i] - last_pos_update[i]).total_seconds() > position_update_thresh: # Skip if lastposupdate is not available
-                              print(f"Skipping velocity mismatch check for {flight_path_name} at index {i} due to out of date position data. ({(times[i] - last_pos_update[i]).total_seconds()}s out of date.)\n")
-                              continue
-                        elif (times[i-vel_sample_size] - last_pos_update[i-vel_sample_size]).total_seconds() > position_update_thresh:
-                              continue # No need to print as the previous check will have already printed the same message
-                        if not replace_vel_with_speed:
-                              if abs(velocity_step - velocity_reported) > vel_mismatch_thresh:
-                                    anomalies["vel_mismatch"]["lats"].append(lats[i-vel_sample_size:i])
-                                    anomalies["vel_mismatch"]["lons"].append(lons[i-vel_sample_size:i])
-                                    print(f"""Reported speed mismatched with distance travelled in {flight_path_name}, between indexes {i-vel_sample_size} and {i}.\n"""
-                                          f"""Reported velocity: {velocity_reported} m/s, required velocity: {velocity_step} m/s, required speed: {speed_step} m/s\n"""
-                                          f"""Time gap: {time_gap} seconds, distance travelled: {distance_travelled} km, displacement: {displacement} km\n""")
-                        else:
-                              if abs(speed_step - velocity_reported) > vel_mismatch_thresh:
-                                    anomalies["vel_mismatch"]["lats"].append(lats[i-vel_sample_size:i])
-                                    anomalies["vel_mismatch"]["lons"].append(lons[i-vel_sample_size:i])
-                                    print(f"""Reported speed mismatched with displacement in {flight_path_name}, between indexes {i-vel_sample_size} and {i}.\n"""
-                                          f"""Reported velocity: {velocity_reported} m/s, required speed: {speed_step} m/s\n"""
-                                          f"""Time gap: {time_gap} seconds, distance travelled: {distance_travelled} km, displacement: {displacement} km\n""")
-      return anomalies
+                        if abs(velocity_step - velocity_reported) > vel_mismatch_thresh:
+                              vel_mismatch_lat.append(lats[i-vel_sample_size:i])
+                              vel_mismatch_lon.append(lons[i-vel_sample_size:i])
+                              print(f"""Reported speed mismatched with distance travelled in {flight_path_name}, between indexes {i-vel_sample_size} and {i}.\n"""
+                                    f"""Reported velocity: {velocity_reported} m/s, required velocity: {velocity_step} m/s, required speed: {speed_step} m/s\n"""
+                                    f"""Time gap: {time_gap} seconds, distance travelled: {distance_travelled} km, displacement: {displacement} km\n""")
+      return vel_mismatch_lat, vel_mismatch_lon
+
 
 def plot_cartopy(flight_paths):
       fig = plt.figure(figsize=(10, 5))
       ax = plt.axes(projection=ccrs.PlateCarree())
       ax.add_feature(cfeature.LAND)
       ax.add_feature(cfeature.OCEAN)
-      ax.add_feature(cfeature.COASTLINE)
+      # ax.add_feature(cfeature.COASTLINE)
       ax.add_feature(cfeature.BORDERS, linestyle=':')
       # ax.add_feature(cfeature.LAKES, alpha=0.5)
       # ax.add_feature(cfeature.RIVERS)
@@ -111,18 +79,21 @@ def plot_cartopy(flight_paths):
 
       ax.set_extent([min_lon, max_lon, min_lat, max_lat], crs=ccrs.PlateCarree())
 
+      max_velocity = max(max(flight_path['velocity']) for flight_path in flight_paths.values())
+      min_velocity = min(min(flight_path['velocity']) for flight_path in flight_paths.values())
+      norm = Normalize(vmin=min_velocity, vmax=max_velocity)
+
       for flight_path_name, flight_path in flight_paths.items():
-            points = plt.scatter(flight_path['lon'], flight_path['lat'], c=range(len(flight_path['lat'])), cmap='viridis', s=5, label=f'{flight_path_name} Points', alpha=1.0)
-            cbar = plt.colorbar(points, orientation='horizontal', pad=0.05, aspect=50, label=f'Index for {flight_path_name}')
+            points = plt.scatter(flight_path['lon'], flight_path['lat'], c=flight_path['velocity'], cmap='viridis', norm=norm, s=5, label=f'{flight_path_name} Points', alpha=1.0)
+            plt.plot(flight_path['lon'], flight_path['lat'], linestyle='-', linewidth=0.5, label=f'{flight_path_name} Path', alpha=0.7)
 
-      plt.scatter(anomalies["neg_time"]["lons"], anomalies["neg_time"]["lats"], c='red', s=10, marker='x', label='Negative Time Anomalies')
-      plt.scatter(anomalies["large_time_gap"]["lons"], anomalies["large_time_gap"]["lats"], c='orange', s=10, marker='x', label='Large Time Gap Anomalies')
-      plt.scatter(anomalies["vel_mismatch"]["lons"], anomalies["vel_mismatch"]["lats"], c='purple', s=10, marker='x', label='Velocity Mismatch Anomalies')
+      cbar = plt.colorbar(points, orientation='horizontal', pad=0.05, aspect=50, label='Velocity')
+      # plt.scatter(vel_mismatch_lon, vel_mismatch_lat, c='red', s=15, marker='x', label='Velocity Mismatch Anomalies')
 
-      plt.legend()
+      # plt.legend()
       plt.show()
 
 
 flight_paths = load_flight_paths()
-anomalies = analyse_data(flight_paths, replace_vel_with_speed=False)
+vel_mismatch_lat, vel_mismatch_lon = analyse_data(flight_paths)
 plot_cartopy(flight_paths)
