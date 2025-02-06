9581c4d331dff7a23af3acebc91c8a0fc2a6dfa4
Max Howard <mdeh2@cam.ac.uk>
2025-02-06 20:15:44 +0000

    added plots to analyse altitude variations

--- plot_flight_path.py
+++ plot_flight_path.py
@@ -16,7 +16,109 @@ TIME_GAP = 60
 CRUISE_STEP = 50 * 1852       # 50 nautical miles in meters
 DELTA_ALT_CRUISE = 3000 * 0.3048    # 3000 ft in meters
 DELTA_ALT_CRUISE = 16000 * 0.3048    # 16000 ft in meters
+FT_TO_M = 0.3048
 AIRPORT_NAMES = pd.read_csv("airports.csv")
+
+def fixed_spacing_floats(s, fmt, nan_value=np.nan):
+    """
+    Return floats contained in given positions of string s
+
+    Parameters
+    ----------
+    s : str
+        String to parse.
+    fmt : list of int
+        Alternating gaps and lengths, starting with a gap.
+    nan_value : ?, optional
+        Value to be returned for failed casts to float. The default is np.nan.
+
+    Raises
+    ------
+    ValueError
+        If len(fmt) < 2.
+
+    Returns
+    -------
+    values : list of float
+        Extracted values.
+    
+    """
+    if len(fmt) < 2:
+        raise ValueError('fmt must have length >= 2')
+    
+    values = []
+    pos = 0
+    for i in range(int(len(fmt)/2)):
+        pos += fmt[i*2]
+        length = fmt[i*2+1]
+        value = s[pos:pos+length]
+        try:
+            value = float(value)
+        except ValueError:
+            value = nan_value
+        values.append(value)
+        pos += length
+    
+    return values
+
+def read_ptf(filepath):
+    """
+    Read a Performance Table File for a given aircraft
+
+    Parameters
+    ----------
+    filepath : str
+        File to read.
+
+    Returns
+    -------
+    ptf : dict
+        Extracted data.
+
+    """
+    ptf = {}
+    with open(filepath, 'r', encoding='cp1252') as f:
+        lines = f.readlines()
+        
+        # Header
+        line = lines[7]
+        ptf['Vcl1'] = float(line[11:14])
+        ptf['Vcl2'] = float(line[15:18])
+        ptf['Mcl'] = float(line[23:27])
+        ptf['mlo'] = float(line[41:])
+        
+        line = lines[8]
+        ptf['Vcr1'] = float(line[11:14])
+        ptf['Vcr2'] = float(line[15:18])
+        ptf['Mcr'] = float(line[23:27])
+        ptf['mnom'] = float(line[41:54])
+        ptf['hMO'] = float(line[71:])
+        
+        line = lines[9]
+        ptf['Vdes1'] = float(line[11:14])
+        ptf['Vdes2'] = float(line[15:18])
+        ptf['Mdes'] = float(line[23:27])
+        ptf['mhi'] = float(line[41:])
+        
+        # Table
+        table = []
+        irow = 0
+        line = lines[16]
+        while line[0] != '=':
+            values = fixed_spacing_floats(line, [0, 3, 4, 3, 3, 5, 1, 5, 1, 5,
+                                                 5, 3, 3, 5, 1, 5, 1, 5, 3, 5,
+                                                 5, 3, 2, 5, 2, 5])
+            table.append(values)
+            irow += 1
+            line = lines[16+irow*2]
+        ptf['table'] = pd.DataFrame(
+            data=table,
+            columns=['FL', 'Vcr', 'flo_cr', 'fnom_cr', 'fhi_cr',
+                     'Vcl', 'ROCDlo_cl', 'ROCDnom_cl', 'ROCDhi_cl', 'fnom_cl',
+                     'Vdes', 'ROCDnom_des', 'fnom_des']
+        ).set_index('FL')
+    
+    return ptf
 
 class FlightPath:
       def __init__(self, flightname:str, df:pd.DataFrame):
@@ -72,6 +174,19 @@ def load_flight_paths(path="./output") -> dict[pd.DataFrame]:
                   df = pd.read_csv(file_path)
                   print(f"Loaded {file} with {len(df)} rows.")
                   flight_paths[file.replace('.csv', '')] = df
+      return flight_paths
+
+def clean_alt_data(flight_paths: dict[pd.DataFrame]):
+      for name, flight_path in flight_paths.items():
+            initial_length = len(flight_path)
+            flight_path = flight_path[flight_path["baroaltitude"].diff().abs() <= 100]
+            mid_length = len(flight_path)
+            flight_path = flight_path[flight_path["geoaltitude"].diff().abs() <= 100]
+            final_length = len(flight_path)
+            if initial_length - final_length > 0:
+                  print(f"Removed {initial_length - mid_length} rows in {name} due to baroaltitude anomalies.")
+                  print(f"Removed {mid_length - final_length} rows in {name} due to geoaltitude anomalies.")
+            flight_paths[name] = flight_path.reset_index(drop=True)
       return flight_paths
 
 
@@ -188,7 +303,10 @@ def analyse_data(flight_paths, time_gap_thresh=60, vel_mismatch_thresh=10, vel_s
       return vel_mismatch_lat, vel_mismatch_lon
 
 
-def plot_cartopy(flight_paths):
+def plot_cartopy(flight_paths, color_by="velocity"):
+      """
+      
+      """
       plt.figure(figsize=(10, 5))
       ax = plt.axes(projection=ccrs.PlateCarree())
       ax.add_feature(cfeature.LAND)
@@ -211,50 +329,32 @@ def plot_cartopy(flight_paths):
 
       ax.set_extent([min_lon, max_lon, min_lat, max_lat], crs=ccrs.PlateCarree())
 
-      alt_measured = "geoaltitude"
-      alt_measured = "baroaltitude"
-
-      max_velocity = max(max(flight_path['velocity']) for flight_path in flight_paths.values())
-      min_velocity = min(min(flight_path['velocity']) for flight_path in flight_paths.values())
-      print(f"Max velocity: {max_velocity}, Min velocity: {min_velocity}")
-
-      # Filter out unrealistic altitude changes
-      for flight_path in flight_paths.values():
-            flight_path[alt_measured] = flight_path[alt_measured].mask(flight_path[alt_measured].diff().abs() > 100)
-
-      max_altitude = max(max(flight_path[alt_measured].dropna()) for flight_path in flight_paths.values())
-      min_altitude = min(min(flight_path[alt_measured].dropna()) for flight_path in flight_paths.values())
-      print(f"Max altitude: {max_altitude}, Min altitude: {min_altitude}")
-
-      # Find the flight path and index where the max altitude occurs
-      for flight_path_name, flight_path in flight_paths.items():
-            if max_altitude in flight_path[alt_measured].values:
-                  max_altitude_index = flight_path[alt_measured].idxmax()
-                  max_altitude_location = (flight_path['lat'].iloc[max_altitude_index], flight_path['lon'].iloc[max_altitude_index])
-                  print(f"Max altitude occurs in flight path {flight_path_name} at index {max_altitude_index}, location: {max_altitude_location}")
-                  break
-
-      cbar_var = "Velocity"
-      cbar_var = "Altitude"
-
-      if cbar_var == "Velocity":
+      if color_by == "velocity":
             cbar_label = "Velocity (m/s)"
+            cbar_source = "velocity"
+            max_velocity = max(max(flight_path['velocity']) for flight_path in flight_paths.values())
+            min_velocity = min(min(flight_path['velocity']) for flight_path in flight_paths.values())
             norm = Normalize(vmin=min_velocity, vmax=max_velocity)
-      elif cbar_var == "Altitude":
-            cbar_label = "Altitude (m)"
+      elif color_by.endswith("altitude"):
+            if color_by == "geoaltitude":
+                  cbar_label = "Geo Altitude (m)"
+                  cbar_source = "geoaltitude"
+            elif color_by == "baroaltitude":
+                  cbar_label = "Baro Altitude (m)"
+                  cbar_source = "baroaltitude"
+            max_altitude = max(max(flight_path[cbar_source]) for flight_path in flight_paths.values())
+            min_altitude = min(min(flight_path[cbar_source]) for flight_path in flight_paths.values())
             norm = Normalize(vmin=min_altitude, vmax=max_altitude)
 
       # Plot the flight paths
       for flight_path_name, flight_path in flight_paths.items():
-            # Plot the path
-            plt.plot(flight_path['lon'], flight_path['lat'], c='red', linestyle='-', linewidth=0.5, alpha=0.7) #, label=f'{flight_path_name} Path'
 
             # Remove the interpolated points before plotting the scatter points
-            flight_path = flight_path[~flight_path['interpolated']]
-            if cbar_var == "Altitude":
-                  c = flight_path[alt_measured]
-            elif cbar_var == "Velocity":
-                  c = flight_path['velocity']
+            if 'interpolated' in flight_path.columns:
+                  plt.plot(flight_path['lon'], flight_path['lat'], c='red', linestyle='-', linewidth=0.5, alpha=0.7) #, label=f'{flight_path_name} Path'
+                  flight_path = flight_path[~flight_path['interpolated']]
+ 
+            c = flight_path[cbar_source]
             points = plt.scatter(flight_path['lon'], flight_path['lat'], c=c, cmap='viridis', norm=norm, s=4, alpha=1.0)
 
       # Plot the interpolated points
@@ -263,12 +363,12 @@ def plot_cartopy(flight_paths):
 
       cbar = plt.colorbar(points, orientation='horizontal', pad=0.05, aspect=50, label=cbar_label)
       cbar.ax.set_position([0.215, -0.05, 0.6, 0.3])  # Adjust the position and size of the colorbar
-      # plt.scatter(vel_mismatch_lon, vel_mismatch_lat, c='red', s=15, marker='x', label='Velocity Mismatched with Distance')
 
       plt.legend()
       plt.show()
 
 def detail_plot(flight_paths):
+      plt.figure(figsize=(10, 5))
       # Define the center as the final lat and lon value of the first flight path
       first_flight_path = next(iter(flight_paths.values()))
       cen_lat = first_flight_path['lat'].iloc[-1]
@@ -335,8 +435,82 @@ def detail_plot(flight_paths):
       plt.savefig("test.png",bbox_inches='tight', dpi = 300)
       plt.show()
 
+def altitude_plot(flight_paths):
+      plt.figure(figsize=(10, 5))
+      for flight_path_name, flight_path in flight_paths.items():
+            time_series = (flight_path['time'] - flight_path["time"][0]) / 60**2
+            plt.plot(time_series, flight_path['geoaltitude'], color="orange", label=f"{flight_path_name} Geo Altitude")
+            plt.scatter(time_series, flight_path['geoaltitude'], color="orange", s=5)
+            plt.plot(time_series, flight_path['baroaltitude'], color="blue", label=f"{flight_path_name} Baro Altitude")
+            plt.scatter(time_series, flight_path['baroaltitude'], color="blue", s=5)
+      plt.xlabel("Time (hours)")
+      plt.ylabel("Altitude (m)")
+      plt.show()
+
+def vert_rate_plot(flight_paths):
+      for flight_path_name, flight_path in flight_paths.items():
+            plt.figure(figsize=(10, 5))
+            avarage_points_count = 60
+            time_series = (flight_path['time'] - flight_path["time"][0]) / 60**2
+            plt.scatter(time_series, flight_path['geoaltitude'].diff() / flight_path["time"].diff(), color="orange", s=5, label="Calculated from Geo Altitude")
+            plt.scatter(time_series, flight_path['baroaltitude'].diff() / flight_path["time"].diff(), color="blue", s=5, label="Calculated from Baro Altitude")
+            plt.scatter(time_series, flight_path["vertrate"], color="green", s=5, label="Reported Vertical Rate")
+            
+            # Calculate and plot the mean value
+            geo_alt_mean = flight_path['geoaltitude'].diff().rolling(window=avarage_points_count).mean() / flight_path["time"].diff().rolling(window=10).mean()
+            baro_alt_mean = flight_path['baroaltitude'].diff().rolling(window=avarage_points_count).mean() / flight_path["time"].diff().rolling(window=10).mean()
+            vert_rate_mean = flight_path["vertrate"].rolling(window=avarage_points_count).mean()
+            plt.plot(time_series, geo_alt_mean, color="orange", linestyle='--', label="Mean Geo Altitude Rate (10 points)")
+            plt.plot(time_series, baro_alt_mean, color="blue", linestyle='--', label="Mean Baro Altitude Rate (10 points)")
+            plt.plot(time_series, vert_rate_mean, color="green", linestyle='--', label="Mean Reported Vertical Rate (10 points)")
+
+            # Calculate and plot the median value
+            geo_alt_median = flight_path['geoaltitude'].diff().rolling(window=avarage_points_count).median() / flight_path["time"].diff().rolling(window=10).median()
+            baro_alt_median = flight_path['baroaltitude'].diff().rolling(window=avarage_points_count).median() / flight_path["time"].diff().rolling(window=10).median()
+            vert_rate_median = flight_path["vertrate"].rolling(window=avarage_points_count).median()
+            plt.plot(time_series, geo_alt_median, color="orange", linestyle='-', label="Median Geo Altitude Rate (10 points)")
+            plt.plot(time_series, baro_alt_median, color="blue", linestyle='-', label="Median Baro Altitude Rate (10 points)")
+            plt.plot(time_series, vert_rate_median, color="green", linestyle='-', label="Median Reported Vertical Rate (10 points)")
+
+            plt.legend()
+            plt.title("Altitude Rate vs Time for " + flight_path_name)
+            plt.xlabel("Time (hours)")
+            plt.ylabel("Altitude Rate (m/s)")
+            plt.ylim(-25, 25)
+            plt.text(0.5, 0.95, 'Vertical rate limited to [-25, 25] m/s', 
+                         horizontalalignment='center', verticalalignment='center', 
+                         transform=plt.gca().transAxes, fontsize=10, color='red')
+            plt.show()
+
+def vert_rate_vs_altitude(flight_paths):
+      for flight_path_name, flight_path in flight_paths.items():
+            plt.figure(figsize=(10, 5))
+            ac_type = flight_path_name.split("_")[2]
+            ptf_filepath = f"BADA/{ac_type}__.ptf"
+            ptf = read_ptf(ptf_filepath)
+            ac_data = ptf['table']
+            time_series = (flight_path['time'] - flight_path["time"][0]) / 60**2
+            plt.scatter(flight_path['geoaltitude'], flight_path["vertrate"], s=5, label="Altitude Rate", c="orange")
+            plt.plot(ac_data.index * 100 * FT_TO_M, ac_data["ROCDhi_cl"] * FT_TO_M/ 60, label="BADA High Load Climb Rate", c = "red")
+            plt.plot(ac_data.index * 100 * FT_TO_M, ac_data["ROCDnom_cl"] * FT_TO_M/ 60, label="BADA Nominal Load Climb Rate", c = "green")
+            plt.plot(ac_data.index * 100 * FT_TO_M, ac_data["ROCDlo_cl"] * FT_TO_M/ 60, label="BADA Low Climb Load Rate", c = "purple")
+            plt.plot(ac_data.index * 100 * FT_TO_M, ac_data["ROCDnom_des"] * FT_TO_M/ -60, label="BADA Nominal Load Descent Rate", c = "blue")
+            plt.xlabel("Altitude (m)")
+            plt.ylabel("Altitude Rate (m/s)")
+            plt.title(f"{flight_path_name} Altitude Rate vs Altitude (geo alt for measured rate)")
+            plt.legend()
+            plt.show()
+
 flight_paths = load_flight_paths("output")
-flight_paths = interpolate_great_circle(flight_paths)
-vel_mismatch_lat, vel_mismatch_lon = analyse_data(flight_paths)
-plot_cartopy(flight_paths)
-detail_plot(flight_paths)
+
+
+
+# flight_paths = interpolate_great_circle(flight_paths)
+# vel_mismatch_lat, vel_mismatch_lon = analyse_data(flight_paths)
+# detail_plot(flight_paths)
+
+flight_paths = clean_alt_data(flight_paths)
+# plot_cartopy(flight_paths, color_by="geoaltitude")
+# altitude_plot(flight_paths)
+vert_rate_plot(flight_paths)
+vert_rate_vs_altitude(flight_paths)
